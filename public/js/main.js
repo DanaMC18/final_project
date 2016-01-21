3f3338f9a8b0763c977ad4cf433060742bf14acb
Dana Czinsky <DanaCzinsky@gmail.com>
2016-01-20 22:16:07 -0500

    creates search.ejs

--- public/js/main.js
+++ public/js/main.js
@@ -2,26 +2,26 @@ console.log('zzzz');
 
 $(document).ready(function(){
 
-  // var displayPosition = function(position) {
-  //   console.log('Latitude: ' + position.coords.latitude + ', Longitude: ' + position.coords.longitude);
-  // }
+  var displayPosition = function(position) {
+    console.log('Latitude: ' + position.coords.latitude + ', Longitude: ' + position.coords.longitude);
+  }
   
-  // var displayError = function(err){
-  //   var errors = {
-  //     1: 'Permission denied',
-  //     2: 'Position unavailable',
-  //     3: 'Request timeout'
-  //   };
-  //   console.log('Error: ' + errors[err.code]);
-  // }
+  var displayError = function(err){
+    var errors = {
+      1: 'Permission denied',
+      2: 'Position unavailable',
+      3: 'Request timeout'
+    };
+    console.log('Error: ' + errors[err.code]);
+  }
   
-  // if (navigator.geolocation) {
-  //   console.log('navigator!!!');
-  //   navigator.geolocation.getCurrentPosition(
-  //     displayPosition, displayError, {enableHighAccuracy: true, timeout: 5000});
-  // } else {
-  //   alert('Geolocation is not supported by this browser')
-  // }
+  if (navigator.geolocation) {
+    console.log('navigator!!!');
+    navigator.geolocation.getCurrentPosition(
+      displayPosition, displayError, {enableHighAccuracy: true, timeout: 5000});
+  } else {
+    alert('Geolocation is not supported by this browser')
+  }
 
   
 
--- public/js/users_controller.js
+++ public/js/users_controller.js
@@ -1,29 +1,5 @@
 angular.module('Napstr').controller('UsersController', UsersController);
 
-
-var displayPosition = function(position) {
-  console.log('Latitude: ' + position.coords.latitude + ', Longitude: ' + position.coords.longitude);
-}
-
-var displayError = function(err){
-  var errors = {
-    1: 'Permission denied',
-    2: 'Position unavailable',
-    3: 'Request timeout'
-  };
-  console.log('Error: ' + errors[err.code]);
-}
-
-if (navigator.geolocation) {
-  console.log('navigator!!!');
-  navigator.geolocation.getCurrentPosition(
-    displayPosition, displayError, {enableHighAccuracy: true, timeout: 5000});
-} else {
-  alert('Geolocation is not supported by this browser')
-}
-
-
-
 function UsersController(){
 
   var ctrl = this;
